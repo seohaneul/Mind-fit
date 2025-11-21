74dfdfe7403f0e50a52dd09f611be2710a2da97c
seohaneul <seo1533618@gmail.com>
2025-11-21 19:55:39 +0900

    수정

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -121,7 +121,6 @@ export default function Dashboard() {
 
     return (
         <div style={{ padding: 16 }}>
-            <LogForm />
             <div style={{ width: "100%", height: 420 }}>
                 <ResponsiveContainer>
                     <BarChart data={chartData} margin={{ top: 20, right: 20, left: 20, bottom: 60 }}>
