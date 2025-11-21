9e3d1efc516db4b717274637a09bd36f4f51781a
seohaneul <seo1533618@gmail.com>
2025-11-21 19:51:52 +0900

    fix : 필요없는 코드 삭제

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import LogForm from "../components/LogForm";
 import Recommendation from "../components/Recommendation";
 import Papa from 'papaparse';
 
